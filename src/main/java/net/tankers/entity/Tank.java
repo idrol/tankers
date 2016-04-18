096d31570009cf81093e364b57648d494eb826b0
idrol <adriangrannas@hotmail.com>
2016-04-18 12:57:48 +0300

    Error fix

--- src/main/java/net/tankers/entity/Tank.java
+++ src/main/java/net/tankers/entity/Tank.java
@@ -9,4 +9,24 @@ public class Tank extends NetworkedEntity {
         super(isServer, instanceID);
         objectIdentifier = "tank";
     }
+
+    @Override
+    public void decodeData(String[] data) {
+
+    }
+
+    @Override
+    public String[] encodeData(String variable) {
+        return new String[0];
+    }
+
+    @Override
+    public void updateServer(float delta) {
+
+    }
+
+    @Override
+    public void updateClient(float delta) {
+
+    }
 }
